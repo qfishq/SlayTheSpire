2156d8c6c82c1d396eab95f93338648dfc2fde7e
AidenJeong8 <111260867+AidenJeong8@users.noreply.github.com>
2025-11-20 08:08:47 -0800

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,12 @@
+[
+  ["mew", 70, "psychic", [3, 100, "fighting"], [5, 200, "fire"]],
+  ["pikachu", 60, "electric", [1, 30, "electric"], [3, 90, "steel"]],
+  ["umbreon", 110, "dark", [2, 60, "psychic"], [4, 150, "dark"]],
+  ["psyduck", 50, "water", [1, 20, "psychic"], [3, 80, "water"]],
+  ["bulbasaur", 70, "grass", [1, 20, "poison"], [3, 70, "grass"]],
+  ["spheal", 70, "water", [1, 20, "ice"], [4, 100, "water"]],
+  ["jigglypuff", 60, "fairy", [1, 20, "sound"], [3, 80, "psychic"]],
+  ["diglett", 50, "ground", [1, 30, "rock"], [3, 70, "ground"]],
+  ["vaporeon", 110, "water", [2, 50, "ice"], [5, 180, "water"]],
+  ["snorlax", 160, "normal", [3, 70, "fighting"], [6, 220, "normal"]]
+]
